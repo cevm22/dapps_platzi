1acfa44330b7ea5d6dc7c4cd2940bd19b90fad3d
CEVM <cevm.12293@hotmail.com>
2022-07-27 18:37:35 -0500

    first deploy PlatziPunks.sol

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,20 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const deploy = async () => {
 
-  /*Aqui se agrega cuanto Eth se le debe enviar al contrato para desplegar, por lo que se le cambió a 0*/
-  const lockedAmount = hre.ethers.utils.parseEther("0");
+    const [deployer] = await ethers.getSigners();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    console.log("Deploying contract with the account: ", deployer.address );
 
-  await lock.deployed();
+    const PlatziPunks = await ethers.getContractFactory("PlatziPunks"); // este debe ser el mismo nombre del smart contract
+    const deployed = await PlatziPunks.deploy();
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
-}
+    console.log("Platzi Punks was deployed at: ", deployed.address)
+};
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+deploy()
+.then(() => process.exit(0))
+.catch(error => {
+    console.log(error)
+    process.exit(1)
 });
