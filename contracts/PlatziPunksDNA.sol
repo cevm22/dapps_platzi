0a871d8030c9123bb7dc5f26d9db191fc6a783f9
CEVM <cevm.12293@hotmail.com>
2022-07-27 21:45:25 -0500

    add pseudorandomDNA to platzipunksdna.sol

--- contracts/PlatziPunksDNA.sol
+++ contracts/PlatziPunksDNA.sol
@@ -201,7 +201,13 @@ contract PlatziPunksDNA {
         "ShortHairTheCaesarSidePart"
     ];
 
-    // TODO: Calculate DNA
+    // this function pseudo random is deterministc and should not be used in production
+    function deterministicPseudoRandomDNA(uint256 _tokenId, address _minter) public pure returns(uint256){
+        uint256 combinedParms = _tokenId + uint160(_minter);
+        bytes memory encodedParams = abi.encodePacked(combinedParms);
+        bytes32 hashedParams = keccak256(encodedParams);
+        return uint256(hashedParams);
+    }
 
     // Get attributes
     uint8 constant ADN_SECTION_SIZE = 2;
