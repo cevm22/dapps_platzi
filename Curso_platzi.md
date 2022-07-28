e9674618e43541c8ad564504295833338523f7d8
CEVM <cevm.12293@hotmail.com>
2022-07-27 22:21:48 -0500

    Smart contracts ended

--- Curso_platzi.md
+++ Curso_platzi.md
@@ -6,3 +6,27 @@ Hardhat es un entorno de desarrollo que permite compilar, probar y depslegar sma
 - npx hardhat test > usa Javascript para hacer tests automatizadas
 
 * Antes de hacer un despliegue, si cuenta con un constructor, se debe configurar los scripts de deploy.js
+
+En caso de que ocurra error por usar muchas variables (mas de 16) https://soliditydeveloper.com/stacktoodeep
+
+* dividirlo en varias funciones
+* usar menos variables
+* Usar brackets y agreagarlo
+contract StackTooDeepTest2 {
+    function addUints(
+        uint256 a,uint256 b,uint256 c,uint256 d,uint256 e,uint256 f,uint256 g,uint256 h,uint256 i
+    ) external pure returns(uint256) {
+        
+        uint256 result = 0;
+        
+        {
+            result = a+b+c+d+e;
+        }
+        
+        {
+            result = result+f+g+h+i;
+        }
+
+        return result;
+    }
+}
--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -14,6 +14,8 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
     uint256 public maxSupply;
     address payable treasury;
 
+    mapping(uint256 => uint256) tokenDNA;
+
     constructor(uint256 _maxSupply, address payable _treasury) ERC721("PlatziPunks","PLPKS"){
         maxSupply = _maxSupply;
         treasury = _treasury;
@@ -24,14 +26,77 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
         require(current < maxSupply, "All PlatziPunks were minted");
         require(msg.value > 1000000000000000, "Must be over 0.001 ETH");
 
+        //generar un DNA con lafuncion  deterministicPseudoRandomDNA
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
+
+        //mintear el nft y enviarlo a quien ejecuta el contrato
         _safeMint(msg.sender, current);
+        // incrementar el id del token
         _idCounter.increment(); // _tokenId.increment()
+        //enviar eth a la tesoreria
         treasury.transfer(msg.value);
 
     }
 
+    // Esta es la base URI que es donde apunta al servidor que guarda la informacion grafica del NFT, y se puede modificar
+    // para que en caso de que el servidor alojado ya no exista, lo puedas reconstruir en otro.
+    // ya que las caracteristicas del NFT estan escritas y guardadas en la blockchain
+    function _baseURI() internal pure override returns(string memory){
+        return "https://avataaars.io/";
+    }
+
+    function  _paramsURI(uint256 _dna) internal view returns(string memory) {
+        string memory params;
+        // Aqui se llena el stack de memoria, por lo que se encierra en llaves "{}" y se saca el la ultima variable
+      {
+        // Esta manera es para concatenar strings
+          params =  string(
+            abi.encodePacked(
+                "accessoriesType=",
+                getAccessoriesType(_dna),
+                "&clotheColor=",
+                getClotheColor(_dna),
+                "&clotheType=",
+                getClotheType(_dna),
+                "&eyeType=",
+                getEyeType(_dna),
+                "&eyebrowType=",
+                getEyeBrowType(_dna),
+                "&facialHairColor=",
+                getFacialHairColor(_dna),
+                "&facialHairType=",
+                getFacialHairType(_dna),
+                "&hairColor=",
+                getHairColor(_dna),
+                "&hatColor=",
+                getHatColor(_dna),
+                "&graphicType=",
+                getGraphicType(_dna),
+                "&mouthType=",
+                getMouthType(_dna),
+                "&skinColor=",
+                getSkinColor(_dna)
+                
+            ));
+      }
+        
+        // finalmente se agrega el ultimo parametro en esta sección
+        return string(abi.encodePacked(params, "&topType=", getTopType(_dna) ));
+    }
+
+    // Aqui se genera el link URL para hacer la visualizacíon del NFT 
+    function imageByDNA(uint256 _dna) public view returns (string memory){
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+
+        return string (abi.encodePacked(baseURI,"?",paramsURI));
+    }
+
     function tokenURI(uint256 tokenId) public view override returns (string memory){
         require(_exists(tokenId),"ERC721 Metadata: URI query for none existent token");
+
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDNA(dna);
 
         // Aqui se comienza a crear una URI para que se almacene toda la info en la blockchain
         // la manera de concatenar es por medio de bytes, como se muesrta a continuacion
@@ -40,7 +105,7 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
                 '{ "name": "PlatziPunks #" ',
                 tokenId,
                 '", "descripcion": "Platzi Punks are randomized avatars stored on chain to teach DAap development on Platzi", "image": ',
-                "// TODO: calculate image url",
+                image,
                 '"}'
             
         ));
